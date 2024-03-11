7a419f348496b6aeae96ad05eae4d0538d60a0a8
fredrickUdom <fredrickudom@gmail.com>
2024-03-11 11:42:59 +0100

    installing react-router-dom and axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import NavBar from './components/navBar/navBar';
 import Homescreen from './screen/homeScreen';
+import {BrowserRouter, Router, Link} from 'react-dom';
 function App() {
   return (
     <div className="App">
