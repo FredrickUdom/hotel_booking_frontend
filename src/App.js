31a1cf48f0b4d9e944477fe970c8213cb5333f30
fredrickUdom <fredrickudom@gmail.com>
2024-03-11 10:44:18 +0100

    creating an homescreen component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import NavBar from './components/navBar/navBar';
+import Homescreen from './screen/homeScreen';
 function App() {
   return (
     <div className="App">
         <NavBar/>
+        <Homescreen/>
     </div>
   );
 }
---
+++ src/screen/homeScreen.js
@@ -0,0 +1,9 @@
+const Homescreen = () => {
+    return ( 
+        <div className="homescreen">
+            <h1>welcome to home screen</h1>
+        </div>
+     );
+}
+ 
+export default Homescreen;
